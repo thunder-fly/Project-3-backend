2627c24acd5341ef3e098cc7580120c94c9b0bc5
parthgpatel <ppatel2015@gmail.com>
2017-08-07 13:22:02 -0400

    added pages and blogs routes

--- config/routes.js
+++ config/routes.js
@@ -11,10 +11,13 @@ module.exports = require('lib/wiring/routes')
 .resources('examples')
 
 // users of the app have special requirements
+
 .post('/sign-up', 'users#signup')
 .post('/sign-in', 'users#signin')
 .delete('/sign-out/:id', 'users#signout')
 .patch('/change-password/:id', 'users#changepw')
 .resources('users', { only: ['index', 'show'] })
+.resources('pages', { except: ['new', 'edit'] })
+.resources('blogs', { except: ['new', 'edit'] })
 
 // all routes created
