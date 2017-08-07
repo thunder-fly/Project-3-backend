35c5d9217d7435c5307802996d93b07543088b9e
jingrid <ijin@bu.edu>
2017-08-07 13:57:46 -0400

    test Blog and Auth CRUD
    All blog and auth CRUD routes and requests successful

---
+++ scripts/blogs/create-blog.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/blogs"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+    "blog": {
+      "text": "'"${TEXT}"'"
+    }
+  }'
+
+echo
