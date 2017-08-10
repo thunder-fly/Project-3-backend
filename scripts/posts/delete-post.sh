7fff4b7e408c1a8cea9725af7842eca762a52748
fcarrion001 <fpc1118@gmail.com>
2017-08-09 20:31:35 -0400

    Can now delete and update posts
    Adjusted errors in config/routes.js for delete and update on post
    Achieved bare minimum functionality on destroy and update methods in the
    controller.
    Created sh files for delete and update for testing
    
    Problems that remain
    ---------
    Anyone with a valid token can create, update, and destroy any post
    Create on post works but produces an error prior to completion
    Error handling can be improved within the delete and update methods
    in the controller
    Code is not modular

---
+++ scripts/posts/delete-post.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/blogs"
+
+curl "${API}${URL_PATH}/${ID}/posts/${POST_ID}" \
+  --include \
+  --request DELETE \
+  --header "Authorization: Token token=$TOKEN"
+
+echo
